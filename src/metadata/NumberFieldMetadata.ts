50eda8b08a940b5016ab24219fa2cc9296250bd3
Rob Wynne <robertowynne@gmail.com>
2022-06-14 21:56:28 +0100

    fix: use field id for filters

--- src/metadata/NumberFieldMetadata.ts
+++ src/metadata/NumberFieldMetadata.ts
@@ -5,6 +5,10 @@ export class NumberFieldMetadata extends AbstractFieldMetadata<number> {
   readonly type = "number"
   deserialize(input: string): number {
     const numberValue: number = parseFloat(input)
+    if(isNaN(numberValue)) {
+      // @ts-ignore
+      return undefined;
+    }
     return numberValue;
   }
   operators = [operator.EQUALS, operator.LESS_THAN, operator.GREATER_THAN];
