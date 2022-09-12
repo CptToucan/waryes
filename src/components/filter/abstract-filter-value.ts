a99941c9ccd9060a24e837979da03c2a912b8624
Rob Wynne <robertowynne@gmail.com>
2022-09-12 21:37:02 +0100

    chore: remove everything

--- src/components/filter/abstract-filter-value.ts
+++
@@ -1,8 +0,0 @@
-import {LitElement} from 'lit';
-import {property} from 'lit/decorators.js';
-import {FilterMetadata} from '../../metadata/FilterMetadata';
-
-export abstract class AbstractFilterValue extends LitElement {
-  @property()
-  filter?: FilterMetadata<unknown>;
-}
---
+++ src/services/firebase.ts
@@ -0,0 +1,34 @@
+import {FirebaseApp, initializeApp} from 'firebase/app';
+import { Firestore, initializeFirestore } from 'firebase/firestore';
+import { getAuth, Auth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAbaxNuNQPWeGj70fil5PJmYJOCeMes6X0',
+  authDomain: 'catur-11410.firebaseapp.com',
+  databaseURL: 'https://catur-11410.firebaseio.com',
+  projectId: 'catur-11410',
+  storageBucket: 'catur-11410.appspot.com',
+  messagingSenderId: '190186563233',
+  appId: '1:190186563233:web:f2d37c452a73424343f2e0',
+  measurementId: 'G-N7T189R144',
+};
+
+class FirebaseServiceClass {
+  init() {
+    const app = initializeApp(firebaseConfig);
+    const db = initializeFirestore(app, {ignoreUndefinedProperties: true});
+    const auth = getAuth();
+
+  
+    this.app = app;
+    this.db = db;
+    this.auth = auth;
+  }
+
+  app?: FirebaseApp;
+  db?: Firestore;
+  auth?: Auth;
+}
+
+const FirebaseService = new FirebaseServiceClass();
+export {FirebaseService};
