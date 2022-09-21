e1df19518701624dd9b3e4ff3deae7292c6e518a
izohek <izohek@gmail.com>
2022-09-20 17:19:18 -0700

    bug: converted registration form to use vaadin-login-form to fix styles and layout.

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -1,5 +1,5 @@
 import {css, html, LitElement, TemplateResult} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import {customElement, property, state} from 'lit/decorators.js';
 import {createUserWithEmailAndPassword} from 'firebase/auth';
 import '@vaadin/email-field';
 import '@vaadin/password-field';
@@ -8,15 +8,10 @@ import '@vaadin/notification';
 import '@vaadin/horizontal-layout';
 import '@vaadin/icon';
 import {FirebaseService} from '../services/firebase';
-import '../components/user-credentials';
-import {CredentialsSubmitEvent} from '../components/user-credentials';
 import {notificationService} from '../services/notification';
 import {Router} from '@vaadin/router';
+import { LoginFormLoginEvent } from '@vaadin/login/vaadin-login-form';
 
-interface RegisterDetails {
-  email: string;
-  password: string;
-}
 
 @customElement('register-route')
 export class RegisterRoute extends LitElement {
@@ -33,38 +28,17 @@ export class RegisterRoute extends LitElement {
         display: flex;
         flex-direction: column;
         justify-content: center;
-        padding: var(--lumo-space-l);
-        max-width: calc(var(--lumo-size-m) * 10);
-      }
-
-      .form-structure {
-        display: flex;
-        flex-direction: column;
-      }
-
-      .form-header {
-        margin: 0;
-        margin-top: calc(
-          var(--lumo-font-size-xxxl) - var(--lumo-font-size-xxl)
-        );
-        color: var(--lumo-header-text-color);
-      }
-
-      vaadin-button {
-        margin-top: 16px;
       }
     `;
   }
 
-  registerDetails: RegisterDetails = {
-    email: '',
-    password: '',
-  };
-
   @property()
   notificationOpened = false;
 
-  async register(event: CredentialsSubmitEvent) {
+  @state()
+  registrationError?: string;
+
+  async register(event: LoginFormLoginEvent) {
     try {
       if (!FirebaseService.auth) {
         throw new Error('Auth service is not initialised');
@@ -72,8 +46,8 @@ export class RegisterRoute extends LitElement {
 
       await createUserWithEmailAndPassword(
         FirebaseService.auth,
-        event.detail.value.email,
-        event.detail.value.password
+        event.detail.username,
+        event.detail.password
       );
 
       notificationService.instance?.addNotification({
@@ -85,35 +59,40 @@ export class RegisterRoute extends LitElement {
       Router.go('/');
     } catch (error: any) {
       console.error(error);
-      let errorMessage = 'Failed to create an account.';
-      if (error.message) {
-        errorMessage = error.message;
-      }
-
-      notificationService.instance?.addNotification({
-        duration: 3000,
-        content: errorMessage,
-        theme: 'error',
-      });
+      this.registrationError = error.message ?? 'Failed to create an account';
     }
   }
+
+  private i18n = () => { return {
+    header: {
+      title: 'Register',
+      description: 'To get access to some of the best features, you need an account. Sign up with an email and password below.',
+    },
+    form: {
+      title: 'Register',
+      username: 'Username',
+      password: 'Password',
+      submit: 'Register',
+      forgotPassword: '',
+    },
+    errorMessage: {
+      title: 'There was a problem with your registration',
+      message: this.registrationError ?? "",
+    },
+    additionalInformation: 'To get access to some of the best features, you need an account. Sign up with an email and password below.',
+  }};
 
   render(): TemplateResult {
     return html`
       <div class="page">
-        <h2 class="form-header">Register</h2>
-        <user-credentials
-          submitLabel="Register"
-          @credentials-submit=${this.register}
-        ></user-credentials>
+        <vaadin-login-form 
+          .i18n=${this.i18n()} 
+          @login=${this.register} 
+          ?error=${this.registrationError !== undefined} 
+          no-forgot-password
+          no-autofocus
+        ></vaadin-login-form>
       </div>
     `;
   }
 }
-
-/**
- *         <span>
-          To get access to some of the best features, you need an account. Sign
-          up with an email and password below.
-        </span>
- */
