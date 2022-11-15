76adab44a5ac6e8945c869377ed7682c548782a0
izohek <izohek@gmail.com>
2022-11-14 17:32:59 -0800

    feat: feature flags + disable firebase auth

--- src/services/router.ts
+++ src/services/router.ts
@@ -1,18 +1,27 @@
 import { Router } from '@vaadin/router';
+import { Features, featureService } from './features';
 
 const router = new Router(document.getElementById("main"));
+
+const routes = [
+  {path: '/', component: 'index-route'},
+  {path: '/units', component: 'units-route'},
+  {path: '/unit/:unitId', component: 'unit-view-route'},
+  {path: '/comparison', component: 'comparison-route'},
+] 
+
+// Features.firebase_auth routes
+if (featureService.enabled(Features.firebase_auth)) {
+  routes.push(
+    {path: '/register', component: 'register-route'},
+    {path: '/login', component: 'login-route'}
+  );
+}
 
 router.setRoutes([
   {path: '/',
     component: 'application-route',
-    children: [
-      {path: '/', component: 'index-route'},
-      {path: '/register', component: 'register-route'},
-      {path: '/login', component: 'login-route'},
-      {path: '/units', component: 'units-route'},
-      {path: '/unit/:unitId', component: 'unit-view-route'},
-      {path: '/comparison', component: 'comparison-route'},
-    ]
+    children: routes
   }
 ]);
 
