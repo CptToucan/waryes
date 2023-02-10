a8c7ff5c52ddd0fccc76f12a2f576e5d5812c122
izohek <izohek@gmail.com>
2023-02-09 21:10:55 -0800

    feat: added sub icons to recon, helicopters, and planes

--- src/utils/get-icon-for-unit.ts
+++ src/utils/get-icon-for-unit.ts
@@ -24,11 +24,38 @@ const iconMap: IconMap = {
 
 export function getIconForUnit(unit: Unit) {
   let icon: string;
-
-  if (unit.specialities && iconMap[unit.specialities[0]] !== undefined) {
-    icon = iconMap[unit.specialities[0]];
-  } else if (unit.category === 'air') {
+  if (unit.category === 'air') {
     icon = 'jet';
+  } else if (unit.category === 'hel') {
+    icon = 'helicopter';
+  } else if (unit.category === 'rec') {
+    icon = 'recon';
+  } else if (unit.specialities && iconMap[unit.specialities[0]] !== undefined) {
+    icon = iconMap[unit.specialities[0]];
+  } else {
+    icon = 'support';
+  }
+
+  return `waryes-svg:${icon}`;
+}
+
+export function getSubIconForUnit(unit: Unit) {
+  let icon: string;
+
+  if (unit.category === 'rec') {
+    if (unit.fuel == null) {
+      icon = iconMap.infantry;
+    } else if (unit.roadSpeed == null) {
+      icon = 'helicopter';
+    } else if (unit.weapons && unit.weapons.length > 0 && (unit.weapons[0].penetration ?? 0) > 11) {
+      icon = unit.weapons[0].ammoDescriptorName.toLocaleLowerCase().includes('atgm')
+        ? 'at'
+        : 'tank';
+    } else {
+      icon = 'transport';
+    }
+  } else if (unit.specialities && iconMap[unit.specialities[0]] !== undefined) {
+    icon = iconMap[unit.specialities[0]];
   } else {
     icon = 'support';
   }
