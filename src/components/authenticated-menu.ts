7887f4ba5418706cf723c722de51bae0f66af36e
Toucan <autotoucan@gmail.com>
2023-04-14 14:37:33 +0100

    feat: improve menu

--- src/components/authenticated-menu.ts
+++ src/components/authenticated-menu.ts
@@ -15,117 +15,150 @@ import {notificationService} from '../services/notification';
 // @ts-ignore
 import WaryesImage from '../../images/waryes-transparent.png';
 import {router} from '../services/router';
-import {ifDefined} from 'lit/directives/if-defined.js';
+// import {ifDefined} from 'lit/directives/if-defined.js';
 import {Features, featureService} from '../services/features';
 import {Unit} from '../types/unit';
-import "./verify-email";
+import './verify-email';
 interface MenuItem {
   name: string;
   icon: string;
   href: string;
 }
 
+interface MenuItemGroup {
+  name: string;
+  items: MenuItem[];
+}
+
 /// Create a single menu item
-type MenuItemRenderer = (menuItem: MenuItem) => TemplateResult;
+type MenuItemRenderer = (menuItem: MenuItem | MenuItemGroup, selected?: boolean) => TemplateResult;
 
 /// Menu definition indicating an ordered list of items per user authentication type
 type MenuDefinition = {
-  user: MenuItem[];
-  guest: MenuItem[];
+  user: (MenuItem | MenuItemGroup)[];
+  guest: (MenuItem | MenuItemGroup)[];
 };
 
 /// Default menu definitions per authentication level.
 const defaultMenu: MenuDefinition = {
   user: [
     {
-      name: 'Search',
-      icon: 'vaadin:search',
-      href: '/',
-    },
-    {
       name: 'Units',
-      icon: 'vaadin:table',
-      href: '/units',
+      items: [
+        {
+          name: 'Search',
+          icon: 'vaadin:search',
+          href: '/',
+        },
+        {
+          name: 'Database',
+          icon: 'vaadin:table',
+          href: '/units',
+        },
+        {
+          name: 'Comparison',
+          icon: 'vaadin:pie-bar-chart',
+          href: '/comparison',
+        },
+      ],
     },
     {
-      name: 'Comparison',
-      icon: 'vaadin:pie-bar-chart',
-      href: '/comparison',
+      name: 'Decks',
+      items: [
+        {
+          name: 'Build',
+          icon: 'vaadin:tools',
+          href: '/deck-builder',
+        },
+        {
+          name: 'Import',
+          icon: 'vaadin:code',
+          href: '/deck-import',
+        },
+        {
+          name: 'Library',
+          icon: 'vaadin:book',
+          href: '/deck-library',
+        },
+        {
+          name: 'My Decks',
+          icon: 'vaadin:folder',
+          href: '/my-decks',
+        },
+      ],
     },
     {
-      name: 'Deck Builder',
-      icon: 'vaadin:tools',
-      href: '/deck-builder',
+      name: 'Information',
+      items: [
+        {
+          name: 'Discord',
+          icon: 'vaadin:comments',
+          href: 'https://discord.gg/gqBgvgGj8H',
+        },
+        {
+          name: 'Privacy Policy',
+          icon: 'vaadin:file-text',
+          href: '/privacy-policy',
+        },
+      ],
     },
-    {
-      name: 'Import Deck',
-      icon: 'vaadin:code',
-      href: '/deck-import',
-    },
-    {
-      name: 'Deck Library',
-      icon: 'vaadin:folder',
-      href: '/deck-library',
-    },
-    {
-      name: 'My Decks',
-      icon: 'vaadin:folder',
-      href: '/my-decks',
-    },
-    {
-      name: 'Discord',
-      icon: 'vaadin:comments',
-      href: 'https://discord.gg/gqBgvgGj8H',
-    },
-    {
-      name: 'Privacy Policy',
-      icon: 'vaadin:file-text',
-      href: '/privacy-policy',
-    }
   ],
   guest: [
     {
-      name: 'Search',
-      icon: 'vaadin:search',
-      href: '/',
-    },
-    {
       name: 'Units',
-      icon: 'vaadin:table',
-      href: '/units',
+      items: [
+        {
+          name: 'Search',
+          icon: 'vaadin:search',
+          href: '/',
+        },
+        {
+          name: 'Database',
+          icon: 'vaadin:table',
+          href: '/units',
+        },
+        {
+          name: 'Comparison',
+          icon: 'vaadin:pie-bar-chart',
+          href: '/comparison',
+        },
+      ],
     },
     {
-      name: 'Comparison',
-      icon: 'vaadin:pie-bar-chart',
-      href: '/comparison',
+      name: 'Decks',
+      items: [
+        {
+          name: 'Build',
+          icon: 'vaadin:tools',
+          href: '/deck-builder',
+        },
+        {
+          name: 'Import',
+          icon: 'vaadin:code',
+          href: '/deck-import',
+        },
+        {
+          name: 'Library',
+          icon: 'vaadin:book',
+          href: '/deck-library',
+        },
+      ],
     },
     {
-      name: 'Deck Builder',
-      icon: 'vaadin:tools',
-      href: '/deck-builder',
+      name: 'Information',
+      items: [
+        {
+          name: 'Discord',
+          icon: 'vaadin:comments',
+          href: 'https://discord.gg/gqBgvgGj8H',
+        },
+        {
+          name: 'Privacy Policy',
+          icon: 'vaadin:file-text',
+          href: '/privacy-policy',
+        },
+      ],
     },
-    {
-      name: 'Import Deck',
-      icon: 'vaadin:code',
-      href: '/deck-import',
-    },
-    {
-      name: 'Deck Library',
-      icon: 'vaadin:folder',
-      href: '/deck-library',
-    },
-
-    {
-      name: 'Discord',
-      icon: 'vaadin:comments',
-      href: 'https://discord.gg/gqBgvgGj8H',
-    },
-
-    {
-      name: 'Privacy Policy',
-      icon: 'vaadin:file-text',
-      href: '/privacy-policy',
-    }
   ],
 };
 
@@ -170,7 +203,6 @@ export class AuthenticatedMenu extends LitElement {
       }
 
       unit-search {
-
         flex: 1 1 0px;
         max-width: 256px;
         min-width: 0px;
@@ -192,6 +224,46 @@ export class AuthenticatedMenu extends LitElement {
           display: none;
         }
       }
+
+      .tabs {
+        display: flex;
+        flex-direction: column;
+        padding-top: var(--lumo-space-s);
+        padding-bottom: var(--lumo-space-s);
+      }
+
+      a.tab {
+        display: flex;
+        padding-top: var(--lumo-space-s);
+        padding-bottom: var(--lumo-space-s);
+        padding-left: var(--lumo-space-m);
+        padding-right: var(--lumo-space-m);
+        text-decoration: none; /* no underline */
+        color: var(--lumo-body-text-color);
+      }
+
+      a.tab:hover {
+        background-color: var(--lumo-contrast-10pct);
+      }
+
+      a.tab:active {
+        background-color: var(--lumo-primary-color-10pct);
+        color: var(--lumo-primary-color);
+      }
+
+      a.tab.selected {
+        background-color: var(--lumo-primary-color-10pct);
+        color: var(--lumo-primary-color);
+      }
+
+      .menu-item-group {
+        padding-left: var(--lumo-space-s);
+        padding-right: var(--lumo-space-s);
+        padding-top: var(--lumo-space-xs);
+        padding-bottom: var(--lumo-space-xs);
+        font-size: var(--lumo-font-size-l);
+        // color: var(--lumo-primary-color);
+      }
     `;
   }
 
@@ -206,39 +278,49 @@ export class AuthenticatedMenu extends LitElement {
   @state()
   selectedMenuItemIndex?: number;
 
+  @state()
+  activeUrl = "/";
+
+
+
   unitSelected(event: CustomEvent) {
     if (event.detail.value as Unit) {
       Router.go(`/unit/${event.detail.value?.descriptorName}`);
     }
   }
 
-  renderMenuItem: MenuItemRenderer = (menuItem) => {
-    return html`
-      <vaadin-tab>
-        <a tabindex="-1" href="${menuItem.href}">
-          <vaadin-icon
-            class="drawer-icon"
-            icon="${menuItem.icon}"
-          ></vaadin-icon>
-          <span>${menuItem.name}</span>
-        </a>
-      </vaadin-tab>
-    `;
+  renderMenuItem: MenuItemRenderer = (menuItem: MenuItem | MenuItemGroup) => {
+    if (isMenuItemGroup(menuItem)) {
+      return html`<div class="menu-item-group">${menuItem.name}</div>
+        ${menuItem.items.map((item) => this.renderMenuItem(item))}`;
+    } else {
+      const activePath = this.activeUrl;
+      const selected = activePath === menuItem.href;
+      return html` <a class="tab ${selected ? "selected" : "" }" href=${menuItem.href}>
+        <vaadin-icon class="drawer-icon" icon=${menuItem.icon}> </vaadin-icon>
+        <div class="menu-item">${menuItem.name}</div>
+      </a>`;
+    }
   };
 
-  firstUpdated() {
-    const activePath = router.location?.route?.path;
-
-    if (activePath) {
-      const menuItems = this.items();
-      const foundIndex = menuItems.findIndex(
-        (item) => item.href === activePath
-      );
-      this.selectedMenuItemIndex = foundIndex;
-    }
+  connectedCallback() {
+    super.connectedCallback();
+    this.handlePopState();
+    this.handlePopState = this.handlePopState.bind(this);
+    window.addEventListener('popstate', this.handlePopState);
   }
 
-  items(): MenuItem[] {
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    window.removeEventListener('popstate', this.handlePopState);
+  }
+
+  handlePopState() {
+    const activePath = router.location?.route?.path;
+    this.activeUrl = activePath || "/";
+  }
+
+  items(): (MenuItem | MenuItemGroup)[] {
     return this.user ? this.menuDefinition.user : this.menuDefinition.guest;
   }
 
@@ -251,16 +333,15 @@ export class AuthenticatedMenu extends LitElement {
     }
 
     items.push({text: 'Settings'});
-    
+
     items.push({text: 'Logout'});
     return items;
   }
 
   async contextMenuItemSelected(event: ContextMenuItemSelectedEvent) {
-    if(event.detail.value.text === "Settings") {
+    if (event.detail.value.text === 'Settings') {
       Router.go('/user-settings');
     }
-
 
     if (event.detail.value.text === 'Logout') {
       const auth = getAuth();
@@ -274,24 +355,22 @@ export class AuthenticatedMenu extends LitElement {
       });
     }
 
-    if(event.detail.value.text === "Verify Email") {
+    if (event.detail.value.text === 'Verify Email') {
       const auth = getAuth();
-      if(auth.currentUser) {
+      if (auth.currentUser) {
         await sendEmailVerification(auth.currentUser);
         notificationService.instance?.addNotification({
           duration: 3000,
           content: 'Verification email sent',
           theme: 'success',
         });
-      }
-      else {
+      } else {
         notificationService.instance?.addNotification({
           duration: 3000,
           content: 'Failed to send verification email',
           theme: 'error',
         });
       }
-
     }
   }
 
@@ -324,11 +403,17 @@ export class AuthenticatedMenu extends LitElement {
   }
 
   render(): TemplateResult {
-    const menu: TemplateResult[] = this.items().map((item) =>
-      this.renderMenuItem(item)
+    const menu: TemplateResult[] = this.items().map((item, index) => {
+      let selected = false;
+      if (index === this.selectedMenuItemIndex) {
+        selected = true;
+      }
+
+      return this.renderMenuItem(item, selected);}
     );
 
-    const shouldMakeUserVerify = this.user && this.user?.emailVerified === false;
+    const shouldMakeUserVerify =
+      this.user && this.user?.emailVerified === false;
 
     return html` <vaadin-app-layout
       style="height: 100%; --vaadin-app-layout-drawer-overlay: true"
@@ -350,15 +435,12 @@ export class AuthenticatedMenu extends LitElement {
           : ''}
       </div>
       <div class="drawer" slot="drawer">
-        <vaadin-tabs
-          orientation="vertical"
-          selected=${ifDefined(this.selectedMenuItemIndex)}
-        >
-          ${menu}
-        </vaadin-tabs>
+        <div class="tabs">${menu}</div>
       </div>
 
-      ${shouldMakeUserVerify ? html`<verify-email></verify-email>` : html`<slot></slot>`}
+      ${shouldMakeUserVerify
+        ? html`<verify-email></verify-email>`
+        : html`<slot></slot>`}
     </vaadin-app-layout>`;
   }
 }
@@ -368,3 +450,30 @@ declare global {
     'authenticated-menu': AuthenticatedMenu;
   }
 }
+
+function isMenuItemGroup(
+  menuItem: MenuItem | MenuItemGroup
+): menuItem is MenuItemGroup {
+  return (menuItem as MenuItemGroup).items !== undefined;
+}
+
+/*
+        <vaadin-tabs
+          orientation="vertical"
+          selected=${ifDefined(this.selectedMenuItemIndex)}
+        >
+          ${menu}
+        </vaadin-tabs>
+
+            return html`
+      <vaadin-tab>
+        <a tabindex="-1" href="${menuItem.href}">
+          <vaadin-icon
+            class="drawer-icon"
+            icon="${menuItem.icon}"
+          ></vaadin-icon>
+          <span>${menuItem.name}</span>
+        </a>
+      </vaadin-tab>
+    `;
+    */
