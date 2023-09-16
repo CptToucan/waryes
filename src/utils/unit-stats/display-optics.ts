a840dd151081e861c185ccadc287a32722f588fa
Toucan <autotoucan@gmail.com>
2023-09-16 14:04:26 +0100

    feat: add air optics

--- src/utils/unit-stats/display-optics.ts
+++ src/utils/unit-stats/display-optics.ts
@@ -1,13 +1,20 @@
 const OPTICS: {[key: number]: string} = {
-  42.45: "Bad",
-  63.675: "Medicore",
-  84.9: "Normal",
-  127.35: "Good",
-  191.025: "Very Good",
-  233.475: "Exceptional"
-}
+  42.45: 'Bad',
+  63.675: 'Mediocre',
+  84.9: 'Normal',
+  127.35: 'Good',
+  191.025: 'Very Good',
+  233.475: 'Exceptional',
+};
 
-export function displayOptics(optics: number ) {
-return OPTICS[optics] || optics;
+export function displayOptics(optics: number) {
+  const opticsValues = Object.keys(OPTICS).map((key) => Number(key));
 
+  for (let i = 0; i < opticsValues.length; i++) {
+    if (optics < opticsValues[i]) {
+      return `${OPTICS[opticsValues[i - 1]]} (${optics})`;
+    }
+  }
+
+  return OPTICS[optics] || optics;
 }
