98b97e9431c9281489cd5b65b75b755d16823e6e
Dustin Cotten <dustincotten@gmail.com>
2024-01-13 23:21:57 -0700

    needs z-index issue fixed and get-icon-for-trait tweaked to use new component

--- src/utils/get-trait-from-icon-name.ts
+++ src/utils/get-trait-from-icon-name.ts
@@ -5,18 +5,21 @@ interface IconTraitMap {
 const iconTraitMap: IconTraitMap = {
     'trait-amphibious' : 
     { 
+        icon: 'trait-amphibious',
         name: "Amphibious", range: 'N/A', 
         activationCondition: 'N/A', 
         effects: ['Can traverse deep waters at 37.5% of the offroad speed']
     },
     'trait-recon':
     { 
+        icon: 'trait-recon',
         name: "Recon", range: 'N/A', 
         activationCondition: 'N/A', 
         effects: ['Deploys 2473m ahead at the start of the game']
     },
     'trait-leader':
     { 
+        icon: 'trait-leader',
         name: "Leader", range: 'Variable', 
         activationCondition: 'Own units nearby', 
         effects: [
@@ -27,24 +30,28 @@ const iconTraitMap: IconTraitMap = {
     },
     'trait-ifv':
     { 
+        icon: 'trait-ifv',
         name: "IFV", range: '318m', 
         activationCondition: 'Own infantry within range', 
         effects: ['-50% suppression']
     },
     'trait-resolute':
     { 
+        icon: 'trait-resolute',
         name: "Resolute", range: 'N/A', 
         activationCondition: 'N/A', 
         effects: ['-15% suppression']
     },
     'parachute':
     { 
+        icon: 'parachute',
         name: "Airborne", range: 'N/A', 
         activationCondition: 'N/A', 
         effects: ['Deploys 3534m ahead at the start of the game']
     },
     'trait-reservist':
     { 
+        icon: 'trait-reservist',
         name: "Reservist", range: 'N/A', 
         activationCondition: 'N/A', 
         effects: [
@@ -56,6 +63,7 @@ const iconTraitMap: IconTraitMap = {
     },
     'trait-cqc':
     { 
+        icon: 'trait-cqc',
         name: "Shock", range: '141m', 
         activationCondition: 'Enemy units within range', 
         effects: [
@@ -67,12 +75,14 @@ const iconTraitMap: IconTraitMap = {
     },
     'trait-special-forces':
     { 
+        icon: 'trait-special-forces',
         name: "Special Forces", range: 'N/A', 
         activationCondition: 'N/A', 
         effects: ['Gains access to improved veterancy table']
     },
     'trait-mp':
     { 
+        icon: 'trait-mp',
         name: "Military Police", range: '265m', 
         activationCondition: 'Own units within range', 
         effects: [
@@ -82,6 +92,7 @@ const iconTraitMap: IconTraitMap = {
     },
     'trait-gsr':
     { 
+        icon: 'trait-gsr',
         name: "GSR", range: 'N/A', 
         activationCondition: 'Immobile', 
         effects: [
@@ -91,18 +102,21 @@ const iconTraitMap: IconTraitMap = {
     },
     'trait-security':
     { 
+        icon: 'trait-security',
         name: "Security", range: 'N/A', 
         activationCondition: 'Immobile', 
         effects: ['+43 optical strength']
     },
     'trait-transport':
     { 
+        icon: 'trait-transport',
         name: "Transport", range: 'N/A', 
         activationCondition: 'N/A', 
         effects: ['can transport owned infantry or towed weapons']
     },
     'trait-sniper':
     { 
+        icon: 'trait-sniper',
         name: "Sniper", range: 'N/A', 
         activationCondition: 'Immobile for 10 seconds', 
         effects: [
@@ -113,6 +127,7 @@ const iconTraitMap: IconTraitMap = {
     },
     'trait-era':
     { 
+        icon: 'trait-era',
         name: "ERA", range: 'N/A', 
         activationCondition: 'N/A', 
         effects: [
@@ -123,6 +138,7 @@ const iconTraitMap: IconTraitMap = {
     },
     'trait-false-flag':
     { 
+        icon: 'trait-false-flag',
         name: "False-Flag", range: 'N/A', 
         activationCondition: 'N/A', 
         effects: ['dangerousness is 0, will be last unit auto-targeted']
